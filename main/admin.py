83218664b0b210f580489dd62038f2543cbb4ce0
nurlight05 <nurlight05@gmail.com>
2022-06-26 15:22:43 +0500

    Namaz and Entry models are added.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Namaz
 
-# Register your models here.
+@admin.register(Namaz)
+class NamazAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'code', 'view_remaining_prayers_count')
+    
+    def view_remaining_prayers_count(self, obj):
+        return 0
+    view_remaining_prayers_count.short_description = 'Remaining prayers'
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
--- main/models.py
+++ main/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
-class Namaz(models.Model):
-    pass
+class Namaz(models.Model):    
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+    code = models.IntegerField(unique=True)
+    
+    class Meta:
+        ordering = ['code']
+        verbose_name_plural = 'namaz'
+        
+    def __str__(self):
+        return self.name
+    
+class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    namaz = models.ForeignKey(Namaz, on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['created_at']
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        return f"{self.user.username} - {self.namaz.name}({self.quantity})"
+    
